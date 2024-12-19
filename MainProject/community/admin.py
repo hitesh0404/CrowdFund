9ad8431ceec01137fb5630d318941380cd393c46
hitesh <hiteshkukaramprajapati@gmail.com>
2024-12-19 11:47:40 +0530

    NEW

--- MainProject/community/admin.py
+++ MainProject/community/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Motive
+from .models import Motive,Group,GroupContributor,InitiatorGroupContributor
 
 @admin.register(Motive)
 class MotiveAdmin(admin.ModelAdmin):
     list_display = ('name', 'logo', 'motive_type', 'created_at')
     list_filter = ('motive_type',) 
-    search_fields = ('name',) 
- 
+    search_fields = ('name',)
 
-
+admin.site.register([Group,GroupContributor,InitiatorGroupContributor]) 
 # admin.site.register(Motive, MotiveAdmin)
--- MainProject/community/models.py
+++ MainProject/community/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from accounts.models import Contributor
+from uuid import uuid4
 """
 Tables  
 
@@ -15,9 +17,9 @@ InitiatorGroupContributor
 
 groupContributor               
                                will contain one foreign key from Contributor
-                               will contain one foreign key from Motive 
+                               will contain one foreign key from Group 
                                role 
-                               inatiator          7738526577       devashi raghunath
+                               inatiator          
 """
 class MotiveType(models.TextChoices):
     SOCIAL_CAUSE = 'Social Cause', 'Social Cause'
@@ -28,8 +30,10 @@ class MotiveType(models.TextChoices):
     OTHER = 'Other', 'Other'
 
 class Motive(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4(),auto_created=True)
     name=models.CharField(max_length=40,unique=True)
-    logo=models.ImageField()
+    # contributor = models.ManyToManyField(Contributor,through='InitiatorGroupContributor')
+    logo=models.ImageField(upload_to="image/motive/logo/")
     motive_type = models.CharField(
         max_length=50,
         choices=MotiveType.choices,
@@ -38,3 +42,44 @@ class Motive(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+class RoleType(models.TextChoices):
+    ADMIN = 'Admin','Admin'
+    CREATOR = 'Creator','Creator'
+    USER = 'User','User'
+
+class Group(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4(),auto_created=True)
+    created_by = models.ForeignKey(Contributor,on_delete=models.DO_NOTHING)
+    dp = models.ImageField(upload_to="image/group/dp/")
+    name = models.CharField(max_length=50,null=False,blank=False,default=id)
+    motive = models.ForeignKey(Motive,on_delete=models.DO_NOTHING)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+class InitiatorGroupContributor(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4(),auto_created=True)
+    contributor = models.ForeignKey(Contributor,on_delete=models.DO_NOTHING)
+    # motive = models.ForeignKey(Motive,on_delete=models.DO_NOTHING)
+    group = models.ForeignKey(Group,on_delete=models.DO_NOTHING)
+    is_active = models.BooleanField(default=True)
+    role = models.CharField( max_length=50,
+        choices=RoleType.choices,
+        default=RoleType.USER,
+        )
+    class Meta:
+        unique_together = ['contributor','group']
+
+class GroupContributor(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4(),auto_created=True)
+    contributor = models.ForeignKey(Contributor,on_delete=models.DO_NOTHING)
+    # motive = models.ForeignKey(Motive,on_delete=models.DO_NOTHING)
+    group = models.ForeignKey(Group,on_delete=models.DO_NOTHING)
+    is_active = models.BooleanField(default=True)
+    role = models.CharField( max_length=50,
+        choices=RoleType.choices,
+        default=RoleType.USER,
+        )
+    class Meta:
+        unique_together = ['contributor','group']
